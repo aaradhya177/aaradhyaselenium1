9a13efcd24525f6a535a99364cfcdac4b0aac6cb
aaradhya177 <aaradhyamehra240@gmail.com>
2026-06-14 13:32:56 +0000

    Final Aaradhya1

--- src/test/java/com/example/SauceDemoTest.java
+++ src/test/java/com/example/SauceDemoTest.java
@@ -16,10 +16,13 @@ public class SauceDemoTest {
 
         ChromeOptions options = new ChromeOptions();
 
+        options.setBinary("/opt/google/chrome/google-chrome");
+
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
 
         WebDriver driver = new ChromeDriver(options);
 
@@ -27,7 +30,6 @@ public class SauceDemoTest {
 
         System.out.println("Website : https://www.saucedemo.com");
         System.out.println("Title : " + driver.getTitle());
-        System.out.println("URL : " + driver.getCurrentUrl());
 
         driver.quit();
 
